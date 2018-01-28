112d6eb05c4174592887aebd0067ec9bdcc66a22
tomJaneson <270635897@qq.com>
2018-01-28 18:32:05 +0800

    '添加银行卡'

--- widget/script/valid.js
+++ widget/script/valid.js
@@ -8,108 +8,100 @@ jQuery.fn.valid = function(newParams){
         }
         var param = Object.assign(defaultParams, newParams)
         var a = this[0]
-        var temparr = []
+        var temparr = {}
         var checkallarr = []
-        var validPower = 3
+        var validPower = 1//其中一项不通过减一
+        var requiredReg = new RegExp('required','g')
+        var dataRegReg = new RegExp('dataReg','g')
+        var checkGroupReg = new RegExp('checkGroup','g')
+        var checkSingle = new RegExp('checkSingle','g')
         for(var i = 0;i<a.length;i++ ){
-            if($(a[i]).attr('data-reg')
-            ||$(a[i]).attr('required')
-            ||$(a[i]).attr('checked')){
-                temparr.push(a[i])
+            if($(a[i]).attr('required')){
+                temparr['required'+i] = a[i]
             }
-            if($(a[i]).attr('type')=='checkbox'){
-                var checktemp = []
-                checktemp.push($(a[i]).attr('name'))
-                checktemp.push($(a[i])[0])
-                checkallarr.push(checktemp)
+            if($(a[i]).attr('data-reg')){
+                temparr['dataReg'+i] = a[i]
+            }
+            if($(a[i]).attr('type')=='checkbox'&&$(a[i]).attr('check-class')){
+                var name = $(a[i]).attr('check-class')
+                if(typeof(temparr['checkGroup'+name])=='undefined'){
+                    temparr['checkGroup'+name] = []
+                }
+                temparr['checkGroup'+name].push(a[i])
+            }
+            if($(a[i]).attr('type')=='checkbox'&&$(a[i]).attr('check-single')){
+                temparr['checkSingle'+i] = a[i]
             }
         }
 
-        var tempPower = 0;
-        $(temparr).each(function(index,elm){
-            //required的
-            if($(this).attr('required')){
-                if($(elm).val()==''){
-                    var requiredMsg = $(this).attr('required-msg')
-                    param.callback(requiredMsg,this)
+        for(var t in temparr){
+            var inputElm = temparr[t]
+
+            // required的
+            if(requiredReg.test(t)){
+                if($(inputElm).val()==''){
+                    var requiredMsg = $(inputElm).attr('required-msg')
+                    param.callback(requiredMsg,inputElm)
                     validPower--
-                    tempPower++
                     return param.isAllCheck
                 }
             }
-            //data-reg的
-            if(!$(this).attr('required')&&$(this).attr('data-reg')){
-                var regstr = $(this).attr('data-reg')
-                var regmsg = $(this).attr('reg-msg')
+
+            // data-reg的
+            if(dataRegReg.test(t)){
+                var regstr = $(inputElm).attr('data-reg')
+                var regmsg = $(inputElm).attr('reg-msg')
                 //分离每个正则
                 var fengereg = new RegExp("&&","gi")
                 if(fengereg.test(regstr)){
                     var regarr = regstr.split('&&')
                     var regMsgArr = regmsg.split('&&')
-                    var that = this;
+                    var tempPower = 1
                     //逐个正则匹配，不匹配则执行回调函数并跳出
                     $(regarr).each(function(i,r){
                         var reg = new RegExp(r,'g')
-                        if(!reg.test($(that).val())){
-                            param.callback(regMsgArr[i],that)
+                        if(!reg.test($(inputElm).val())){
+                            param.callback(regMsgArr[i],inputElm)
                             validPower--
-                            tempPower++
+                            tempPower--
                             return param.isAllCheck
                         }
                     })
+                    if(tempPower<1){return false}//检验不通过，里面的循环跳出，外面循环跟住跳出
                 }else{
                     var regsingle = new RegExp(regstr,'gi')
                     if(!regsingle.test($(this).val())){
                         param.callback(regmsg,this)
                         validPower--
-                        tempPower++
                         return param.isAllCheck
                     }
                 }
             }
 
-        })
-
-        //checked的
-        // 验证完required和data-reg后开始杨峥checked
-        if(tempPower==0||param.isAllCheck){
-            // 整理checkbox的数组
-            var checktemp2 = [];
-            var checkarr = [];
-            if(checkallarr.length!=0){
-                $(checkallarr).each(function(index,elm){
-                    if(checktemp2.length==0){
-                        checktemp2.push(elm)
-                    }
-                    if(checkallarr[index+1]!=undefined){
-                        if(checkallarr[index][0]==checkallarr[index+1][0]){
-                            checktemp2.push(checkallarr[index+1])
-                        }else{
-                            checkarr.push(checktemp2)
-                            checktemp2 = [];
-                        }
-                    }else{
-                        checkarr.push(checktemp2)
-                    }
-                })
-            }
-
-            $(checkarr).each(function(index,elm){
+            // checkGroup的
+            if(checkGroupReg.test(t)){
                 var msgswitch = 0;
-                $(elm).each(function(i,e){
-                    // console.log($(e))
-                    if($(e[1]).prop('checked')){msgswitch++}
+                $(inputElm).each(function(index,elm){
+                    if($(elm).prop('checked')){msgswitch++}
                 })
                 if(msgswitch==0){
-                    var checkedMsg = '至少勾选'+$(elm[0][1]).attr('checkclass')+'中的一项';
-                    param.callback(checkedMsg,this)
+                    var checkedMsg = '至少勾选'+$(inputElm[0]).attr('check-class')+'中的一项';
+                    param.callback(checkedMsg,inputElm)
                     validPower--
                     return param.isAllCheck
                 }
-            })
+            }
+
+            //checkSingle的
+            if(checkSingle.test(t)){
+                var checkSingleMsg = $(inputElm).attr('check-single-msg')
+                if(!$(inputElm).prop('checked')){
+                    param.callback(checkSingleMsg,inputElm);
+                    validPower--
+                    return param.isAllCheck
+                }
+            }
+
         }
-
-
-
-        if(validPower<3){return false}else{return true}
+        if(validPower<1){return false}else{return true}
     }
